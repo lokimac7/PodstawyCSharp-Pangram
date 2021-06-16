6db27387fb94499ee2e66f60b112c7e4a2ad0e5e
MACIEJ CZERWINSKI <MACIEJ.CZERWINSKI@BEAUTYOFSCIENCE.PL>
2021-06-16 14:23:38 +0200

    Pangram_v1

---
+++ Pangram/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Pangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pangram p = new Pangram();
+            Console.WriteLine(p.IsPangram("The quick brown fox jumps over the lazy dog"));
+            Console.WriteLine(p.IsPangram("I am programmer"));
+        }
+        public class Pangram
+        {
+            string allLetters = "qwertyuiopasdfghjklzxcvbnm";
+            public bool IsPangram(string sentence)
+              {
+                int count = 0;
+                bool ans = true;
+                sentence = sentence.ToLower();           
+                if (sentence.Length > 1000)
+                {
+                    sentence = sentence.Substring(0, 1000);
+                }
+
+                foreach (var item in allLetters)
+                {
+                    foreach (var item2 in sentence)
+                    {
+                        if (item != item2)
+                            count++;
+                        else
+                            break;
+                    }
+                    if (count == sentence.Length)
+                    {
+                        ans = false;
+                        break;
+                    }
+                    else
+                        count = 0;
+                }
+                return ans;
+            }
+        }
+    }
+}
